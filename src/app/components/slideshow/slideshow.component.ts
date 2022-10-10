54b314d1ff5b736350e8bbce3e585188326f4f89
ibellon <isidro.bellon@gmail.com>
2022-10-10 20:32:03 +0200

    Slideshow de películas

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,17 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/cartelera-response';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.css']
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, AfterViewInit {
 
   @Input() movies: Movie[];
 
   constructor() { 
     this.movies = [];
+  }
+  ngAfterViewInit(): void {
+    const swiper = new Swiper('.swiper-container', {
+      loop: true,
+    });    
   }
 
   ngOnInit(): void {
