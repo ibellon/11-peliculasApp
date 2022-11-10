6e2f0809339ef99e848541db105b280610f13d00
ibellon <isidro.bellon@gmail.com>
2022-11-10 20:15:10 +0100

    rxjs-CombineLatest y Animate.css

--- src/app/pages/pelicula/pelicula.component.ts
+++ src/app/pages/pelicula/pelicula.component.ts
@@ -1,6 +1,7 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { combineLatest } from 'rxjs/internal/observable/combineLatest';
 import { Cast } from 'src/app/interfaces/credits-response';
 import { MovieResponse } from 'src/app/interfaces/movie-response';
 import { PeliculasService } from 'src/app/services/peliculas.service';
@@ -24,19 +25,32 @@ export class PeliculaComponent implements OnInit {
     const id = this.activatedRoute.snapshot.params['id'];
     //const {original_title, release_date} = this.activatedRoute.snapshot.params;
     //console.log(id, original_title, release_date);
-    this.peliculaService.getPeliculaDetalle(id).subscribe(result => {
-      if(!result){
-        this.router.navigateByUrl("/home");
-        return;
+
+    combineLatest([
+      this.peliculaService.getPeliculaDetalle(id),
+      this.peliculaService.getCredits(id)
+    ]).subscribe(([pelicula, creditos]) => {
+      if(!pelicula){
+          this.router.navigateByUrl("/home");
+          return;
       }
-      //console.log(result);
-      this.pelicula = result;
+      this.pelicula = pelicula;
+      this.cast = creditos.filter( actor => actor.profile_path !== null);
     });
 
-    this.peliculaService.getCredits(id).subscribe(result => {
-      console.log(result);
-      this.cast = result;
-    })
+    // this.peliculaService.getPeliculaDetalle(id).subscribe(result => {
+    //   if(!result){
+    //     this.router.navigateByUrl("/home");
+    //     return;
+    //   }
+    //   //console.log(result);
+    //   this.pelicula = result;
+    // });
+
+    // this.peliculaService.getCredits(id).subscribe(result => {
+    //   console.log(result);
+    //   this.cast = result.filter( actor => actor.profile_path !== null);
+    // })
   }
 
   onRegresar() {
